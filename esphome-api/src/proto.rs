f585fe8d75193de18d96709a18d4d2fc939f0191
Josh Kropf <josh@slashdev.ca>
2026-01-04 01:42:41 -0500

    POC for encrypted esphome messaging

--- esphome-api/src/proto.rs
+++ esphome-api/src/proto.rs
@@ -24,6 +24,7 @@ use std::io::{BufRead, Write};
 
 use anyhow::{Result, anyhow};
 use prost::{Message, bytes::{Buf, BufMut, Bytes, BytesMut}, encoding::{decode_varint, encode_varint}};
+use snow::TransportState;
 
 #[derive(Debug)]
 struct Frame {
@@ -65,9 +66,20 @@ fn encode_message<M, B>(message: &M, buffer: &mut B) -> Result<()>
     Ok(())
 }
 
-pub fn read_message<R>(stream: &mut R) -> Result<ProtoMessage>
-    where R: BufRead
+fn encode_encrypted_message<M, B>(message: &M, buffer: &mut B) -> Result<()>
+    where M: Message + MessageId, B: BufMut
 {
+    let message_id = M::ID as u16;
+    let message_len = message.encoded_len() as u16;
+
+    buffer.put_u16(message_id);
+    buffer.put_u16(message_len);
+    message.encode(buffer)?;
+
+    Ok(())
+}
+
+pub fn read_message<R: BufRead>(stream: &mut R) -> Result<ProtoMessage> {
     let buf = stream.fill_buf()?;
     if buf.len() == 0 {
         return Err(anyhow!("Read zero bytes expecting frame"));
@@ -111,7 +123,109 @@ pub fn write_message<S, M>(stream: &mut S, message: &M) -> Result<()>
 
     let buf = buffer.freeze();
     let sz = stream.write(&buf)?;
-    println!("Write {} bytes", sz);
+    println!("Write frame {} - {:02x?}", buf.len(), &buf[..]);
 
     Ok(())
+}
+
+pub fn write_encrypted_frame<S: Write>(stream: &mut S, payload: &[u8]) -> Result<()> {
+    let mut buffer = BytesMut::new();
+
+    buffer.put_u8(1);
+    buffer.put_u16(payload.len() as u16);
+    buffer.put(payload);
+
+    let buf = buffer.freeze();
+    stream.write_all(&buf)?;
+    println!("Write frame {} - {:02x?}", buf.len(), &buf[..]);
+
+    Ok(())
+}
+
+pub fn write_handshake_reject<S: Write>(stream: &mut S, reason: &str) -> Result<()> {
+    let mut payload = vec![0x01];
+    payload.extend_from_slice(reason.as_bytes());
+
+    write_encrypted_frame(stream, payload.as_slice())
+}
+
+pub fn write_hello_frame<S: Write>(stream: &mut S,  node_name: &str, mac_addr: &str) -> Result<()> {
+    let mut payload = vec![0x01];
+    payload.extend_from_slice(node_name.as_bytes());
+    payload.push(0);
+    payload.extend_from_slice(mac_addr.as_bytes());
+    payload.push(0);
+
+    write_encrypted_frame(stream, payload.as_slice())
+}
+
+pub fn write_encrypted_message<S, M>(stream: &mut S, transport: &mut TransportState, message: &M) -> Result<()>
+    where S: Write, M: Message + MessageId
+{
+    let mut message_buffer = BytesMut::with_capacity(512);
+    encode_encrypted_message(message, &mut message_buffer)?;
+
+    let buf = message_buffer.freeze();
+    println!("Message for write {} - {:02x?}", buf.len(), &buf[..]);
+
+    let mut buffer = vec![0u8; 512];
+    let len = transport.write_message(&buf, &mut buffer)?;
+
+    write_encrypted_frame(stream, &buffer[..len])?;
+
+    Ok(())
+}
+
+pub fn read_encrypted_frame<R: BufRead>(stream: &mut R) -> Result<Bytes, ProtoError> {
+    let buf = stream.fill_buf()?;
+    if buf.len() == 0 {
+        return Err(ProtoError::ReadZero);
+    }
+
+    let mut buffer = Bytes::copy_from_slice(buf);
+    println!("Frame buffer {} - {:02x?}", buf.len(), buf);
+
+    let byte_zero = buffer.get_u8();
+    if byte_zero != 1 {
+        return Err(ProtoError::InvalidIndicator(1, byte_zero));
+    }
+
+    let message_size = buffer.get_u16() as usize;
+    stream.consume(3);
+
+    let buf = stream.fill_buf()?;
+    if buf.len() < message_size {
+        return Err(ProtoError::BufferUnderrun(buf.len(), message_size));
+    }
+
+    let buffer = Bytes::copy_from_slice(&buf[..message_size]);
+    stream.consume(message_size);
+
+    Ok(buffer)
+}
+
+pub fn read_message_encrypted<R: BufRead>(stream: &mut R, transport: &mut TransportState) -> Result<ProtoMessage> {
+    let frame = read_encrypted_frame(stream)?;
+
+    let mut buffer = vec![0u8; 512];
+    let len = transport.read_message(&frame, &mut buffer)?;
+
+    let mut buffer = Bytes::copy_from_slice(&buffer[..len]);
+
+    let message_type = buffer.get_u16() as u64;
+    let message_size = buffer.get_u16();
+
+    ProtoMessage::decode(message_type, &mut buffer)
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum ProtoError {
+    #[error("Error reading from stream")]
+    ReadError(#[from] std::io::Error),
+    #[error("Read zero bytes expecting frame")]
+    ReadZero,
+    #[error("Expected first byte of frame to be {0}, found {1}")]
+    InvalidIndicator(u8, u8),
+    #[error("Buffer underrun; buf {0}, message {1}")]
+    BufferUnderrun(usize, usize),
 }
--- esphome-api/src/server.rs
+++ esphome-api/src/server.rs
@@ -18,7 +18,8 @@
 
 use std::{io::{BufReader, Write}, net::{TcpListener, TcpStream}};
 
-use anyhow::Result;
+use anyhow::{Result, anyhow};
+use base64::prelude::*;
 
 use crate::proto::*;
 
@@ -121,7 +122,88 @@ pub fn start_server<H: RequestHandler>(handler: H) -> Result<()> {
         println!("Connection established");
         let stream = stream?;
 
-        handle_connection(&handler, stream)?;
+        handle_connection_encrypted(&handler, stream)?;
+    }
+
+    Ok(())
+}
+
+fn handle_connection_encrypted<H: RequestHandler>(handler: &H, stream: TcpStream) -> Result<()> {
+    let noise_psk = BASE64_STANDARD.decode("jfD5V1SMKAPXNC8+d6BvE1EGBHJbyw2dSc0Q+ymNMhU=")?;
+    let noise_psk: [u8; 32] = noise_psk.try_into().unwrap();
+
+    let mut noise = snow::Builder::new("Noise_NNpsk0_25519_ChaChaPoly_SHA256".parse()?)
+        // do I need prologue?
+        .prologue(b"NoiseAPIInit\0\0")?
+        .psk(0, &noise_psk)?
+        .build_responder()?;
+
+    let mut reader = BufReader::new(stream);
+
+    // https://developers.esphome.io/architecture/api/protocol_details/
+
+    let frame1 = match read_encrypted_frame(&mut reader) {
+        Err(ProtoError::InvalidIndicator(1, 0)) => {
+            write_handshake_reject(&mut reader.get_ref(), "Bad indicator byte")?;
+            println!("Sent invalid frame to client... disconnect");
+            return Ok(());
+        }
+        r => r
+    }?;
+
+    // First frame is NOISE_HELLO; zero length
+    println!("Frame1 {:02x?}", &frame1[..]);
+    if frame1.len() > 0 {
+        return Err(anyhow!("I expected first frame after connect to be zero length"));
+    }
+
+    write_hello_frame(&mut reader.get_ref(), "test_device", "00:00:00:00:00:01")?;
+
+    let frame2 = read_encrypted_frame(&mut reader)?;
+
+    // TODO is static buffer necessary?
+    let mut buffer = vec![0u8; 512];
+    // let mut buffer = BytesMut::new();
+    match noise.read_message(&frame2[1..], &mut buffer) {
+        Err(snow::Error::Decrypt) => {
+            write_handshake_reject(&mut reader.get_ref(), "Handshake MAC failure")?;
+            println!("Sent handshake failed to client... disconnect");
+            return Ok(());
+        }
+        r => r
+    }?;
+
+    // let mut buffer = BytesMut::new();
+    let len = noise.write_message(&[], &mut buffer)?;
+    println!("Noise write {}", len);
+
+    let mut payload = vec![0x00];
+    payload.extend_from_slice(&buffer[..len]);
+
+    write_encrypted_frame(&mut reader.get_ref(), &payload)?;
+    println!("Sent handshake success");
+
+    let mut transport = noise.into_transport_mode()?;
+    loop {
+        let request = read_message_encrypted(&mut reader, &mut transport)?;
+        println!("Request: {:?}", request);
+
+        let mut stream = reader.get_ref();
+
+        match request {
+            ProtoMessage::HelloRequest(_) => {
+                write_encrypted_message(&mut stream, &mut transport, &HelloResponse {
+                    // Mirrored API version from HA 2025.12.3
+                    // This seems reasonable since that's what I'm developing against
+                    api_version_major: 1,
+                    api_version_minor: 13,
+                    // I don't see server_info or name in HA dashboard anywhere
+                    server_info: "My Server Info".to_string(),
+                    name: "My Server Name".to_string()
+                })?
+            }
+            _ => { }
+        }
     }
 
     Ok(())
