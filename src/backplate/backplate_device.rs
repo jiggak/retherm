c7ac484c0522909b771bb04dfcb4762362bb84a8
Josh Kropf <josh@slashdev.ca>
2026-01-25 02:07:42 -0500

    Backplate integration (#5)
    * Adding separate backplate impl for running dev mode
    
    * Make nest backplate a device only feature
    
    * Auto reconnect backplate when unplugged
    
    * Mark hvac control complete in readme
    
    * Minor code cleanup

---
+++ src/backplate/backplate_device.rs
@@ -0,0 +1,124 @@
+/*
+ * ReTherm - Home Assistant native interface for Gen2 Nest thermostat
+ * Copyright (C) 2026 Josh Kropf <josh@slashdev.ca>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use std::{sync::mpsc::{Receiver, Sender, channel}, thread, time::Duration};
+
+use anyhow::Result;
+use nest_backplate::{BackplateCmd, BackplateConnection, BackplateError, BackplateResponse, Wire};
+
+use crate::{backplate::{HvacAction, HvacControl}, events::{Event, EventSender}};
+
+pub struct DeviceBackplateThread {
+    cmd_sender: Sender<BackplateCmd>
+}
+
+impl DeviceBackplateThread {
+    pub fn start<S>(dev_path: &'static str, event_sender: S) -> Result<Self>
+        where S: EventSender + Send + 'static
+    {
+        let (cmd_sender, cmd_receiver) = channel();
+
+        // Should I have spearate read/write threads?
+        // With a single thread, I am relying on the backplate to send a message
+        // before I can send one back. Maybe that's OK though, since the backplate
+        // seems to constanty send messages.
+        thread::spawn(move || {
+            loop {
+                match backplate_main_loop(dev_path, &event_sender, &cmd_receiver) {
+                    Ok(()) => unreachable!("Backplate message loop should not exit with Ok"),
+                    Err(error) => {
+                        if let Some(error) = error.downcast_ref::<BackplateError>() {
+                            if let BackplateError::IoError(error) = error {
+                                println!("{}", error);
+                                println!("Error in backplate thread, attempting reconnect");
+                                thread::sleep(Duration::from_secs(1));
+                                continue;
+                            }
+                        }
+
+                        Err::<(), anyhow::Error>(error)
+                    }
+                }.expect("Backplate thread error");
+            }
+        });
+
+        Ok(Self {
+            cmd_sender
+        })
+    }
+}
+
+fn backplate_main_loop<S: EventSender>(
+    dev_path: &str,
+    event_sender: &S,
+    cmd_receiver: &Receiver<BackplateCmd>
+) -> Result<()> {
+    let mut backplate = BackplateConnection::open(dev_path)?;
+
+    // This triggers a constant stream of messages
+    backplate.send_command(BackplateCmd::StatusRequest)?;
+
+    loop {
+        match backplate.read_message()? {
+            BackplateResponse::Climate { temperature, .. } => {
+                event_sender.send_event(Event::SetCurrentTemp(temperature))?;
+            }
+            BackplateResponse::WireSwitched(wire, state) => {
+                println!("Wire:{:?} state:{}", wire, state);
+            }
+            msg => {
+                println!("{:?}", msg);
+            }
+        }
+
+        if let Ok(cmd) = cmd_receiver.try_recv() {
+            backplate.send_command(cmd)?;
+        }
+    }
+}
+
+impl HvacControl for DeviceBackplateThread {
+    fn switch_hvac(&self, action: &HvacAction) -> Result<()> {
+        let cmds = match action {
+            HvacAction::Heating => {
+                [
+                    BackplateCmd::SwitchWire(Wire::W1, true),
+                    BackplateCmd::SwitchWire(Wire::Y1, false)
+                ]
+            }
+            HvacAction::Cooling => {
+                [
+                    BackplateCmd::SwitchWire(Wire::W1, false),
+                    BackplateCmd::SwitchWire(Wire::Y1, true)
+                ]
+            }
+            HvacAction::Idle => {
+                [
+                    BackplateCmd::SwitchWire(Wire::W1, false),
+                    BackplateCmd::SwitchWire(Wire::Y1, false)
+                ]
+            }
+        };
+
+        for cmd in cmds {
+            self.cmd_sender.send(cmd)?;
+        }
+
+        Ok(())
+    }
+}
