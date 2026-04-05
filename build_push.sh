0e86a90666daf74ed7ebdb6059c6063f43e283e9
Josh Kropf <josh@slashdev.ca>
2026-04-05 16:52:50 -0400

    Use env var for host to receive build push

--- build_push.sh
+++ build_push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-HOST=nest-dev
+if [ -z "${NEST_HOST}" ]; then
+   echo "Set NEST_HOST to hostname of device to receive build" >&2
+   exit 1
+fi
 
 EXAMPLE=$1
 if [ "${EXAMPLE}" != "" ]; then
@@ -13,5 +16,5 @@ fi
 
 if cargo build ${ARGS} --target=armv7-unknown-linux-gnueabihf --release; then
    echo "Sending build via netcat"
-   cat ${OUTPUT} | nc -q0 ${HOST} 51234
+   cat ${OUTPUT} | nc -q0 ${NEST_HOST} 51234
 fi
