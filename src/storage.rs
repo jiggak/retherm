e12974c96684a4ad68f2503f69b38a963521f7ca
Josh Kropf <josh@slashdev.ca>
2026-06-13 11:51:05 -0400

    Move state writes to separate thread

--- src/storage.rs
+++ src/storage.rs
@@ -16,11 +16,11 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, sync::mpsc::{Sender, channel}, thread};
 
 use anyhow::{Result, anyhow};
 use log::{info, warn};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
 
 use crate::{
     config::Config,
@@ -29,7 +29,8 @@ use crate::{
 };
 
 pub struct Storage {
-    state_file_path: PathBuf
+    state_storage: StorageBackend,
+    write_thread: Sender<ThermostatState>
 }
 
 impl Storage {
@@ -39,18 +40,17 @@ impl Storage {
         if !state_dir.is_dir() {
             Err(anyhow!("Directory {:?} does not exist", state_dir))
         } else {
+            let state_storage = StorageBackend::new(config.state_file_path.clone());
+            let write_thread = start_write_thread(state_storage.clone());
             Ok(Self {
-                state_file_path: config.state_file_path.clone()
+                state_storage, write_thread
             })
         }
     }
 
     pub fn read_state(&self) -> Result<ThermostatState> {
-        info!("Loading state from {:?}", self.state_file_path);
-        let state = if self.state_file_path.is_file() {
-            let toml_src = fs::read_to_string(&self.state_file_path)?;
-            let state_storage:StoredState = toml::from_str(&toml_src)?;
-            ThermostatState::from(&state_storage)
+        let state = if let Some(state) = self.state_storage.read()? {
+            ThermostatState::from(&state)
         } else {
             warn!("State does not exist, using default");
             ThermostatState::default()
@@ -60,27 +60,32 @@ impl Storage {
 
         Ok(state)
     }
+}
 
-    fn save_state(&self, state: &ThermostatState) -> Result<()> {
-        info!("Saving state to {:?}", self.state_file_path);
-        let state_storage = StoredState::from(state);
-        let toml_src = toml::to_string(&state_storage)?;
-        fs::write(&self.state_file_path, toml_src)?;
-        Ok(())
-    }
+fn start_write_thread(state_storage: StorageBackend) -> Sender<ThermostatState> {
+    let (tx, rx) = channel::<ThermostatState>();
+
+    thread::spawn(move || {
+        while let Ok(state) = rx.recv() {
+            let state = StoredState::from(&state);
+            state_storage.write(state).unwrap();
+        }
+    });
+
+    tx
 }
 
 impl EventHandler for Storage {
     fn handle_event(&mut self, event: &Event) -> Result<()> {
         if let Event::State(state) = event {
-            self.save_state(&state)?;
+            self.write_thread.send(state.clone())?;
         }
 
         Ok(())
     }
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, PartialEq)]
 struct StoredState {
     target_temp: f32,
     current_temp: f32,
@@ -105,5 +110,46 @@ impl From<&StoredState> for ThermostatState {
             mode: value.mode,
             ..Default::default()
         }
+    }
+}
+
+#[derive(Clone)]
+struct StorageBackend {
+    file_path: PathBuf
+}
+
+impl StorageBackend {
+    fn new(file_path: PathBuf) -> Self {
+        Self { file_path }
+    }
+
+    fn read<T>(&self) -> Result<Option<T>>
+        where T: DeserializeOwned
+    {
+        info!("Loading file {:?}", self.file_path);
+
+        let state = if self.file_path.is_file() {
+            let toml_src = fs::read_to_string(&self.file_path)?;
+            Some(toml::from_str(&toml_src)?)
+        } else {
+            None
+        };
+
+        Ok(state)
+    }
+
+    fn write<T>(&self, value: T) -> Result<()>
+        where T: Serialize + DeserializeOwned + PartialEq
+    {
+        if let Some(existing) = self.read::<T>()? {
+            if existing != value {
+                info!("Saving to file {:?}", self.file_path);
+
+                let toml_src = toml::to_string(&value)?;
+                fs::write(&self.file_path, toml_src)?;
+            }
+        }
+
+        Ok(())
     }
 }
